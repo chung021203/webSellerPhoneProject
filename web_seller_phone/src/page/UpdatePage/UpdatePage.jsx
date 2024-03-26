0a1c5bf3c86c1070e32d5edc88afe47ad6eb7ebb
TMChi3n <tranminhchien15052003@gmail.com>
2024-03-26 15:33:50 +0700

    update frontend

--- web_seller_phone/src/page/UpdatePage/UpdatePage.jsx
+++ web_seller_phone/src/page/UpdatePage/UpdatePage.jsx
@@ -1,25 +1,13 @@
-import '../UpdatePage/Update.css'
+import '../UpdatePage/Update.css';
 
 function UpdatePage() {
- return (
-    <div className="container-insert">
+    return (
+        <div className="container-insert">
             <div>
                 <form id="add_form">
                     <h2 id="title">Cập Nhật Sản Phẩm</h2>
                     <label>Tên điện thoại</label>
-                    <div className="input-container">
-                        <input type="text" id="product_name" placeholder="Nhập tên điện thoại" />
-                        <select id="brand" name="brand">
-                            <option value="Apple">Thương hiệu</option>
-                            <option value="Apple">Apple</option>
-                            <option value="Samsung">Samsung</option>
-                            <option value="Samsung">Oppo</option>
-                            <option value="Samsung">Huawei</option>
-                            <option value="Samsung">Vivo</option>
-                            <option value="Samsung">Xiaomi</option>
-                            <option value="Samsung">Google Pixel</option>
-                        </select>
-                    </div>
+                    <input type="text" id="product_name" placeholder="Nhập tên điện thoại" />
                     <br />
                     <label>Giá tiền</label>
                     <input type="text" id="product_price" placeholder="Nhập giá tiền" /> <br />
@@ -29,7 +17,7 @@ function UpdatePage() {
                 </form>
             </div>
         </div>
- )
+    );
 }
 
-export default UpdatePage
+export default UpdatePage;
