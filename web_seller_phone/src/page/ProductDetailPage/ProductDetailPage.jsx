e55f2753b480d6f4b88719e42e0f9f6417fd5b68
chungle021203 <lechung021203@gmail.com>
2024-03-27 15:18:27 +0700

    Hoàn thành trang chi tiết sp

--- web_seller_phone/src/page/ProductDetailPage/ProductDetailPage.jsx
+++ web_seller_phone/src/page/ProductDetailPage/ProductDetailPage.jsx
@@ -1,5 +1,14 @@
+import ProductDetailComponent from '../../Components/ProductDetailComponent/ProductDetailComponent';
+
 function ProductDetailPage() {
-    return <h2>ProductDetailPage</h2>;
+    return (
+        <div style={{ width: '100%', height: '100vh', background: '#f5f5fa' }}>
+            <h4>Trang chủ</h4>
+            <div style={{ width: '1270px', height: '100%', margin: '0 auto' }}>
+                <ProductDetailComponent />
+            </div>
+        </div>
+    );
 }
 
 export default ProductDetailPage;
