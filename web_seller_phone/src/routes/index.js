1b0f9a55bd662d4cb258d1f03647268f195f94f1
chungle <chung0le21203@ail.lemle>
2024-04-01 15:28:26 +0700

    fix bug lỗi page

--- web_seller_phone/src/routes/index.js
+++ web_seller_phone/src/routes/index.js
@@ -3,19 +3,19 @@ import OrderPage from '../page/OrderPage/OrderPage';
 import ProductPage from '../page/ProductPage/ProductPage';
 import CartPage from '../page/CartPage/CartPage';
 import NotFoundPage from '../page/NotFoundPage/NotFoundPage';
-import CheckoutPage from '../page/CheckoutPage';
-import InsertPage from '../page/InsertPage';
-import UpdatePage from '../page/UpdatePage';
+import UpdatePage from '../page/UpdatePage/UpdatePage';
+import InsertPage from '../page/InsertPage/InsertPage';
+import CheckoutPage from '../page/CheckoutPage/CheckoutPage';
 
 const publicRoutes = [
     { path: '/', component: HomePage },
     { path: '/order', component: OrderPage },
     { path: '/products', component: ProductPage },
     { path: '/cart', component: CartPage },
-    { path: '/checkout', component: CheckoutPage },
     { path: '*', component: NotFoundPage, layout: null },
-    { path: '/product/add', component: InsertPage },
-    { path: '/product/update', component: UpdatePage },
+    { path: '/update', component: UpdatePage },
+    { path: '/insert', component: InsertPage },
+    { path: '/checkout', component: CheckoutPage },
 ];
 const privateRoutes = [];
 export { privateRoutes, publicRoutes };
