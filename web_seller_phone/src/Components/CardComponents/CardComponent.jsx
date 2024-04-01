48c6a48bef389d3acffa4db8c0694acd18383bcb
chungle <chung0le21203@ail.lemle>
2024-04-01 16:29:51 +0700

    thêm style cho card component

--- web_seller_phone/src/Components/CardComponents/CardComponent.jsx
+++ web_seller_phone/src/Components/CardComponents/CardComponent.jsx
@@ -1,36 +1,28 @@
-import React from "react"
-import Meta from 'antd/lib/card/Meta'
-import { Card } from 'antd'
-import { StyleNameProduct} from './style'
-import { WrapperReportText} from './style'
-import {WrapperPriceText} from './style'
-import { WrapperDiscountText} from './style'
-import {WrapperImageStyle} from './style'
-
-
+import React from 'react';
+import { Card } from 'antd';
+import { StyleNameProduct } from './style';
+import { WrapperReportText } from './style';
+import { WrapperPriceText } from './style';
+import { WrapperDiscountText } from './style';
 
 const CardComponent = () => {
     return (
         <Card
-        hoverable
-        headStyle={ {width: '200px', height: '200px'}}
-        style={{ width: 200 }}
-        bodyStyle={{ padding: '10px'}}
-        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-      >
-    
-        <StyleNameProduct>IPhone</StyleNameProduct>
-        <WrapperReportText>
-            <span>4.96</span>
-            <span>| đã bán 1000+</span>
-        </WrapperReportText>
-        <WrapperPriceText>
-            1000000đ
-           <WrapperDiscountText>
-            -5%
-           </WrapperDiscountText>
-        </WrapperPriceText>
-      </Card>
-    )
-}
-export default CardComponent
+            hoverable
+            style={{ width: '200px', boxShadow: '0 4px 6px #00000029' }}
+            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+        >
+            <StyleNameProduct>IPhone</StyleNameProduct>
+            <WrapperReportText>
+                <span>4.96</span>
+                <span>| đã bán 1000+</span>
+            </WrapperReportText>
+            <WrapperPriceText>
+                1000000đ
+                <WrapperDiscountText>-5%</WrapperDiscountText>
+            </WrapperPriceText>
+        </Card>
+    );
+};
+
+export default CardComponent;
