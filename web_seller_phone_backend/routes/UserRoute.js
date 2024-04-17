8aad5304a4ec2d893616f61530e1bba23159ad0f
TMChi3n <tranminhchien15052003@gmail.com>
2024-04-17 11:51:58 +0700

    List users and fix login

--- web_seller_phone_backend/routes/UserRoute.js
+++ web_seller_phone_backend/routes/UserRoute.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import {
-    createUser,loginUser
-} from '../controller/UserController.js';
+import { createUser, loginUser, getAllUser } from '../controller/UserController.js';
 
 const router = express.Router();
 router.post('/post/user', (req, res) => createUser(req, res));
 router.post('/post/login', (req, res) => loginUser(req, res));
+router.get('/get/users', (req, res) => getAllUser(req, res));
 export default router;
